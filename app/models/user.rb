10b120dae6ae386aeb7faaca65602e92c7060a51
yosuke0517 <x.clover.x007@gmail.com>
2019-08-14 19:04:36 +0900

    twitterログイン（画像取得できない）

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,7 @@ class User < ApplicationRecord
     has_many :authorizations
     has_secure_password(validations: false)
     has_one_attached :image
-    #has_many_attached :image
+    #has_many_attached :images
     #before_save { self.email = email.downcase }
     before_save :email_downcase
     validates :name, presence: true, length: {maximum: 50}
@@ -21,7 +21,7 @@ class User < ApplicationRecord
             user.provider = auth[:provider]
             user.uid = auth[:uid]
             user.name = auth[:info][:name]
-            #user.image = auth[:info][:image]
+            user.image = auth[:info][:image]
             user.token = auth[:credentials][:token]
             user.secret = auth[:credentials][:secret]
         end
