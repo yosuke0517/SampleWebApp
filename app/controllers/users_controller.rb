336ccf551607378daf3738bfda2e52c263d111af
yosuke0517 <x.clover.x007@gmail.com>
2019-08-20 18:59:57 +0900

    ログインしていない時フォローボタンを表示しないよう修正

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,12 +6,10 @@ class UsersController < ApplicationController
   end
 
   def show
-    @user = User.find(params[:id])
-    @questions = @user.questions.where(user_id: @user.id)
-    #@answers = Answer.where(user_id: @user.id)
-    @answers = Answer.includes(:question).where(user_id: @user)
-    #@question_ids = @answers.pluck(:question_id)
-    #@answered_questions = Question.where(id: @question_ids)
+    # @user = User.find(params[:id])
+    # @questions = @user.questions.where(user_id: @user)
+    # @answers = Answer.includes(:question).where(user_id: @user)
+    @user = User.includes(:questions, :answers).find(params[:id])
     @followings = @user.followings.page(params[:page])
     @followers = @user.followers.page(params[:page])
   end
